77ec81693a50339ef303f1317c9e5b315112d9e7
Nam Phung <namphung@Nams-MacBook-Pro.local>
2017-12-02 05:04:06 -0600

    added to PlayerAI, implemented heuristic()

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -12,8 +12,27 @@ public class PlayerAI extends BaseAI {
         return board.getAvailableMoves().size() > 0;
     }
 
-    public int heuristic(Board board) {
-        return -1;
+    public double heuristic(Board board) {
+        int numEmptyCell = board.getAvailableCells().size();
+        int sumCells = 0;
+
+        for (int[] row : board.getMap()) {
+            int sumRow = 0;
+            for (int cell : row) {
+                sumRow += cell;
+            }
+
+            sumCells += sumRow;
+        }
+
+        int averageCellsValue = sumCells / (16 - numEmptyCell);
+        int monotonicity = monotonicity(board);
+
+        int emptyValue = (numEmptyCell != 0) ? (int)(Math.log(numEmptyCell) / Math.log(2)) : 0;
+        int largestPositionScore = largestTilePositionScore(board);
+        int smoothness = smoothness(board);
+
+        return monotonicity + emptyValue * 2.5 + board.getMaxTile() + 0.1 * smoothness;
     }
 
     public int monotonicity(Board board) {
@@ -41,5 +60,42 @@ public class PlayerAI extends BaseAI {
         }
 
         return Math.max(Math.max(monotonicity[0], monotonicity[1]), Math.max(monotonicity[2], monotonicity[3]));
+    }
+
+    public int largestTilePositionScore(Board board) {
+        int score = 0;
+        int maxVal = board.getMaxTile();
+
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                if (board.getCellValue(row, col) == maxVal) {
+                    if ((row == 0 || row == 3) && (col == 0 || col == 3)) {
+                        score += 10;
+                    } else {
+                        score -= 10;
+                    }
+                }
+            }
+        }
+
+        return score;
+    }
+
+    public int smoothness(Board board) {
+        int score = 0;
+
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                int cellVal = board.getCellValue(row, col);
+                int up = (row > 0) ? board.getCellValue(row - 1, col) : 0;
+                int down = (row < 3) ? board.getCellValue(row + 1, col) : 0;
+                int left = (col > 0) ? board.getCellValue(row, col - 1) : 0;
+                int right = (col < 3) ? board.getCellValue(row, col + 1) : 0;
+
+                score += Math.min(Math.min(Math.abs(cellVal - left), Math.abs(cellVal - right)), Math.min(Math.abs(cellVal - up), Math.abs(cellVal - down)));
+            }
+        }
+
+        return score * -1;
     }
 }
